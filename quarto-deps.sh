3459110b5780f4ced5a6afb84738a03b67e9a42f
Jinming Ren <3191293752@qq.com>
2025-07-13 16:42:24 +0800

    quarto CLI update

--- quarto-deps.sh
+++ quarto-deps.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 # Install Quarto CLI
-wget https://github.com/quarto-dev/quarto-cli/releases/download/v1.7.32/quarto-1.7.32-linux-amd64.deb
-sudo dpkg -i quarto-1.7.32-linux-amd64.deb
+wget https://github.com/quarto-dev/quarto-cli/releases/download/v1.7.32/quarto-1.7.32-linux-amd64.tar.gz
+tar -xvzf quarto-1.7.32-linux-amd64.tar.gz
+export PATH=$PATH:$(pwd)/quarto-1.7.32-linux-amd64/bin
+# Render webpage (incremental rendering with cache and freezer)
+quarto render --use-freezer
+cd tinyml
+quarto render --use-freezer
+cd ..
