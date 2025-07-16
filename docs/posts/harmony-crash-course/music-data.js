c0addaefd3b41424879e5d03ca3f1163d2bec17e
Jinming Ren <3191293752@qq.com>
2025-07-16 18:35:26 +0800

    Music theory add

---
+++ docs/posts/harmony-crash-course/music-data.js
@@ -0,0 +1,15 @@
+// Music data for harmony crash course
+const MUSIC_DATA = {
+    cooleys: `X: 1
+T: Cooley's
+M: 4/4
+L: 1/8
+K: Emin
+|:D2|"Em"EB{c}BA B2 EB|~B2 AB dBAG|"D"FDAD BDAD|FDAD dAFD|
+"Em"EBBA B2 EB|B2 AB defg|"D"afe^c dBAF|"Em"DEFD E2:|
+|:gf|"Em"eB B2 efge|eB B2 gedB|"D"A2 FA DAFA|A2 FA defg|
+"Em"eB B2 eBgB|eB B2 defg|"D"afe^c dBAF|"Em"DEFD E2:|`,
+
+    // Add more tunes here as needed
+    // example: `X: 2...`,
+};
---
+++ posts/music-theory-crash-course/instruments-reference.js
@@ -0,0 +1,73 @@
+// MIDI Instrument Reference for abcjs
+// Use these numbers in the createMusicPlayer() function
+
+// === PIANO ===
+// 0 = acoustic_grand_piano
+// 1 = bright_acoustic_piano
+// 2 = electric_grand_piano
+// 3 = honky_tonk_piano
+// 4 = electric_piano_1
+// 5 = electric_piano_2
+
+// === STRINGS ===
+// 24 = acoustic_guitar_nylon
+// 25 = acoustic_guitar_steel
+// 26 = electric_guitar_jazz
+// 27 = electric_guitar_clean
+// 32 = acoustic_bass
+// 33 = electric_bass_finger
+// 40 = violin
+// 41 = viola
+// 42 = cello
+// 43 = contrabass
+// 45 = tremolo_strings
+// 46 = pizzicato_strings
+// 47 = orchestral_harp
+
+// === WINDS ===
+// 56 = trumpet
+// 57 = trombone
+// 58 = tuba
+// 59 = muted_trumpet
+// 60 = french_horn
+// 64 = soprano_sax
+// 65 = alto_sax
+// 66 = tenor_sax
+// 67 = baritone_sax
+// 68 = oboe
+// 69 = english_horn
+// 70 = bassoon
+// 71 = clarinet
+// 72 = piccolo
+// 73 = flute
+// 74 = recorder
+// 75 = pan_flute
+
+// === PERCUSSION ===
+// 112 = tinkle_bell
+// 113 = agogo
+// 114 = steel_drums
+// 115 = woodblock
+// 116 = taiko_drum
+// 117 = melodic_tom
+// 118 = synth_drum
+
+// === ETHNIC ===
+// 104 = sitar
+// 105 = banjo
+// 106 = shamisen
+// 107 = koto
+// 108 = kalimba
+// 109 = bag_pipe
+// 110 = fiddle
+
+// === POPULAR EXAMPLES ===
+/*
+Piano: 0
+Guitar: 24
+Violin: 40
+Flute: 73
+Trumpet: 56
+Saxophone: 65
+Harp: 47
+*/
