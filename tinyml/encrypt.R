ae29a7139efc7a9c5a02603d4d6d64dd24e745c6
Jinming Ren <3191293752@qq.com>
2025-11-22 18:16:37 +0800

    Change PW and add slack definition

--- tinyml/encrypt.R
+++ tinyml/encrypt.R
@@ -4,7 +4,7 @@ staticryptR::staticryptr(
   # files     = "../docs/tinyml/eda-3d-placement/", # 要加密的目录
   files = c("../docs/tinyml/eda-3d-placement/eda-3d-placement.html"), 
   directory = "../docs/tinyml/eda-3d-placement/",  # 用 “.” 表示在输出目录内部替换文件
-  password  = "142857",         # 建议长度较长
+  password  = "yubei",         # 建议长度较长
   short     = TRUE,                    # 如果密码长度太短可设 TRUE
   # recursive = TRUE,                    # 遍历子目录
   # 可选：定制加密页面的外观
